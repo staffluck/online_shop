cd2879a774aae4789b4c410e6e53f1c5d5447db7
Arseny Leontev <agent47daun@gmail.com>
2022-03-09 00:02:49 +0400

    Добавление Предмета Продукта к ППродукту

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ProductListCreateView
+from .views import ProductListCreateView, ProductItemAddToProductView
 
 urlpatterns = [
-    path("", ProductListCreateView.as_view(), )
+    path("", ProductListCreateView.as_view(), ),
+    path("<int:pk>/add_item/", ProductItemAddToProductView.as_view(), )
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.exceptions import NotFound, PermissionDenied
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .serializers import ProductSerializer
+from .serializers import ProductItemSerializer, ProductSerializer
 from .models import Product, ProductItem, Deal
 
 class ProductListCreateView(ListCreateAPIView):
@@ -10,3 +13,20 @@ class ProductListCreateView(ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class ProductItemAddToProductView(APIView):
+
+    def post(self, request, pk):
+        try:
+            product = Product.objects.select_related("owner").get(id=pk)
+        except Product.DoesNotExist:
+            raise NotFound()
+        if not product.owner == request.user:
+            raise PermissionDenied()
+
+        serializer = ProductItemSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(product=product)
+
+        return Response(serializer.data, 200)
