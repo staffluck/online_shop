637dc30eeed6c246739fdaeea31153d1e7d0d043
agent47daun <agent47daun@gmail.com>
2022-05-07 22:49:20 +0400

    Вынос работы с mail в отдельное приложение common

---
+++ common/mail.py
@@ -0,0 +1,11 @@
+from typing import Union
+
+from celery import shared_task
+from django.core.mail import send_mail as django_send_mail
+from django.conf import settings
+
+
+@shared_task(name="send_mail")
+def send_mail(send_to: Union[tuple, list], subject: str, message: str) -> None:
+    from_email = settings.EMAIL_HOST_USER
+    django_send_mail(subject, message, from_email, send_to)
